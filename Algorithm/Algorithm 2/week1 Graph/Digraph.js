74572b8563d935288c315149fda718acdad8df77
zhengyuanjie <sj82516@gmail.com>
2016-04-29 23:59:05 +0800

    update digraph

--- Algorithm/Algorithm 2/week1 Graph/Digraph.js
+++ Algorithm/Algorithm 2/week1 Graph/Digraph.js
@@ -1,3 +1,181 @@
-var Digraph = function(){
-	
+/**
+ * @param {size} - number of Directed graph vertices
+ */
+var Digraph = function(size){
+	this.size = size;
+	this.adj = [];
+	function init(){
+		for (var i = 0; i < this.size; i++) {
+			this.adj.push([]);
+		};
+	}
+	init.bind(this)();
+}
+
+Digraph.prototype.addEdges = function(v,w){
+	this.adj[v].push(w);
+}
+
+Digraph.prototype.adj = function(v){
+	return this.adj[v];
+}
+
+/**
+ * Digraph implement by DFS
+ * @param {Digraph}
+ * @param {Int} - start point
+ */
+var DirectedDFS = function(graph,s){
+	this.graph = graph;
+	this.mark = [];
+	this.edgeTo = [];
+	function init(){
+		for (var i = 0; i < this.graph.size; i++) {
+			this.mark.push(false);
+			this.edgeTo.push(null);
+		};
+		this.dfs(s)
+	}
+	init.bind(this)();
+}
+
+DirectedDFS.prototype.dfs = function(v){
+	var next = 0;
+	this.mark[v] = true;
+	for(var i = 0; i < this.graph.adj[v].length ; i++){
+		next = this.graph.adj[v][i];
+		if(!this.mark[next]){
+			this.edgeTo[next] = v;
+			this.dfs(next);
+		}
+	}
+}
+
+/**
+ * @return {array} - show all vertices that start vertex can reach
+ */
+DirectedDFS.prototype.reachablility = function(){
+	var reachablility = [];
+	for(var i=0; i < this.mark.length; i++){
+		if(this.mark[i]){
+			reachablility.push(i);
+		}
+	}
+	return reachablility;
+}
+
+/**
+ * Digraph implemented by BFS , can accept multi-sources
+ * @param {[type]}
+ * @param {[type]}
+ */
+var DirectedBFS = function(graph,args){
+	this.graph = graph;
+	this.mark = [];
+	this.edgeTo = [];
+	this.args = args;
+	this.argsNum = args.length;
+	//distance
+	this.distTo = [];
+	function init(){
+		for (var i = 0; i < this.graph.size; i++) {
+			this.mark.push([]);
+			this.edgeTo.push([]);
+			this.distTo.push([]);
+			for(var j = 0; j < this.argsNum; j++){
+				this.mark[i].push(false);
+				this.edgeTo[i].push(null);
+				this.distTo[i].push(null);
+			}
+		};
+		for(var k = 0; k < this.argsNum; k++){
+			this.bfs(args[k],k);	
+		}
+	}
+	init.bind(this)();
+}
+
+DirectedBFS.prototype.bfs = function(v,k){
+	var queue = [],
+		dist = 0,
+		curr = 0,
+		next = 0;
+	this.edgeTo[v][k] = v;
+	this.distTo[v][k] = 0;
+	this.mark[v][k] = true;
+	queue.push(v);
+	while(queue){
+		curr = queue.shift();
+		if(curr===undefined){
+			return;
+		}
+		for(var i = 0; i < this.graph.adj[curr].length; i++){
+			next = this.graph.adj[curr][i];
+			if(!this.mark[next][k]){
+				queue.push(next);
+				this.edgeTo[next][k] = curr;
+				this.distTo[next][k] = this.distTo[curr][k]+1;
+				this.mark[next][k] = true;
+			}
+		}
+	}
+}
+
+//show in reverse order
+DirectedBFS.prototype.multiSourceShortestPath = function(v){
+	var path = [],
+		minDist = Number.MAX_VALUE,
+		minArgsIndex = 0;
+	for(var i = 0; i < this.argsNum; i++){
+		if(this.distTo[v][i]===null){
+			continue;
+		}
+		if(minDist > this.distTo[v][i]){
+			minDist = this.distTo[v][i];
+			minArgsIndex = i;
+		}
+	}
+	while(v!==this.args[minArgsIndex]){
+		path.push(v);
+		v = this.edgeTo[v][minArgsIndex]; 
+	}
+	path.push(this.args[minArgsIndex]);
+	return path;
+}
+
+var Topology = function(graph){
+	this.graph = graph;
+	this.size = graph.size;
+	this.stack = [];
+	this.mark = [];
+	function init(){
+		for (var i = 0; i < this.graph.size; i++) {
+			this.mark.push(false);
+		};
+		for (var i = 0; i < this.graph.size; i++) {
+			if(!this.mark[i]){
+				this.dfs(i);
+			}
+		};
+	}
+	init.bind(this)();
+}
+
+Topology.prototype.dfs = function(v){
+	this.mark[v] = true;
+	var next = 0;
+	for(var i=0; i < this.graph.adj[v].length; i++){
+		next = this.graph.adj[v][i];
+		if(!this.mark[next]){
+			this.dfs(next);
+		}
+	}
+	this.stack.push(v);
+}
+
+module.exports = {
+	Digraph : Digraph,
+	DirectedDFS : DirectedDFS,
+	DirectedBFS : DirectedBFS,
+	Topology : Topology
 }
--- Algorithm/Algorithm 2/week1 Graph/DigraphSpec.js
+++ Algorithm/Algorithm 2/week1 Graph/DigraphSpec.js
@@ -1,35 +1,58 @@
 var expect = require('chai').expect;
-var Graph = require('./Graph.js').Graph;
-var Path = require('./Graph.js').Path;
-var CC = require('./Graph.js').CC;
+var Digraph = require('./Digraph.js').Digraph;
+var DirectedDFS = require('./Digraph.js').DirectedDFS;
+var DirectedBFS = require('./Digraph.js').DirectedBFS;
+var Topology = require('./Digraph.js').Topology;
 
 //use eql() instead of equal()
-describe('Undirected Graph Implement By DFS', function(){
-	var graph = new Graph(13); 
-	graph.addEdges(0,5);
-	graph.addEdges(4,3);
-	graph.addEdges(0,1);
-	graph.addEdges(9,12);
-	graph.addEdges(6,4);
-	graph.addEdges(5,4);
-	graph.addEdges(0,2);
-	graph.addEdges(11,12);
-	graph.addEdges(9,10);
-	graph.addEdges(0,6);
-	graph.addEdges(7,8);
-	graph.addEdges(9,11);
-	graph.addEdges(5,3);
-	it('Undirected Graph Implement By DFS Has Path To', function(){
-		var path = new Path(graph,0,true);
-		expect(path.hasPathTo(2)).to.be.equal(true);
+describe('Directed Graph Implement By DFS', function(){
+	var diGraph = new Digraph(13); 
+	diGraph.addEdges(4,2);
+	diGraph.addEdges(2,3);
+	diGraph.addEdges(3,2);
+	diGraph.addEdges(6,0);
+	diGraph.addEdges(0,1);
+	diGraph.addEdges(2,0);
+	diGraph.addEdges(11,12);
+	diGraph.addEdges(12,9);
+	diGraph.addEdges(9,10);
+	diGraph.addEdges(9,11);
+	diGraph.addEdges(8,9);
+	diGraph.addEdges(10,12);
+	diGraph.addEdges(11,4);
+	diGraph.addEdges(4,3);
+	diGraph.addEdges(3,5);
+	diGraph.addEdges(6,8);
+	diGraph.addEdges(8,6);
+	diGraph.addEdges(5,4);
+	diGraph.addEdges(0,5);
+	diGraph.addEdges(6,4);
+	diGraph.addEdges(6,9);
+	diGraph.addEdges(7,6);
+	it('Directed Graph Implement By DFS Has Path To', function(){
+		var directedDFS = new DirectedDFS(diGraph,0);
+		expect(directedDFS.reachablility()).to.eql([0,1,2,3,4,5]);
+
 	});
-	it('Undirected Graph Implement By BFS Path To', function(){
-		var path = new Path(graph,0,false);
-		expect(path.edgeTo).to.be.eql([0,0,0,5,5,0,0,null,null,null,null,null,null]);
+	it('Directed Graph Implement By BFS in Multi-Source Has Path To', function(){
+		var directedBFS = new DirectedBFS(diGraph,[1,7,10]);
+		expect(directedBFS.multiSourceShortestPath(5)).to.eql([5,0,6,7]);
+	});
+	var diGraph2 = new Digraph(7); 
+	diGraph2.addEdges(0,5);
+	diGraph2.addEdges(0,2);
+	diGraph2.addEdges(0,1);
+	diGraph2.addEdges(3,6);
+	diGraph2.addEdges(3,5);
+	diGraph2.addEdges(3,4);
+	diGraph2.addEdges(5,2);
+	diGraph2.addEdges(6,4);
+	diGraph2.addEdges(6,0);
+	diGraph2.addEdges(3,2);
+	diGraph2.addEdges(1,4);
+	it('Topology sort', function(){
+		var topology = new Topology(diGraph2);
+		console.log(topology.stack);
 	});
 
-	it('Undirected Graph Implement Connected Component', function(){
-		var cc = new CC(graph);
-		expect(cc.count).to.be.equal(3);
-	});
 })
