fac222a0fbda6426d341c486614bc80e148de950
zhengyuanjie <sj82516@gmail.com>
2016-05-23 23:45:43 +0800

    create index min PQ

---
+++ Algorithm/Algorithm 2/week2 Weighted Graph/IndexMinPQ.js
@@ -0,0 +1,22 @@
+var IndexMinPQ = function(size){
+	this.size = size;
+	this.length = 0;
+	this.pq = [];
+	this.qp = [];
+	this.keys = [];
+}
+
+IndexMinPQ.prototype = {
+	insert : function(){
+
+	},
+	deleteMin : function(){
+
+	},
+	contains : function(){
+
+	},
+	decreaseKey : function(){
+
+	}
+}
