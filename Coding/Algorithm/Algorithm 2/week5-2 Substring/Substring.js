6fd03173ce6247c0999ecc5cf9365c079cc7943f
zhengyuanjie <sj82516@gmail.com>
2016-06-21 22:23:00 +0800

    update substring

--- Coding/Algorithm/Algorithm 2/week5-2 Substring/Substring.js
+++ Coding/Algorithm/Algorithm 2/week5-2 Substring/Substring.js
@@ -33,12 +33,66 @@ KMP.prototype = {
   }
 }
 
-var BM = function(){
-
+var BM = function(pat){
+  this.right = [];
+  this.pat = pat;
+  this.patLength = pat.length;
+  for(let i=0; i<R; i++){
+    this.right.push(-1);
+  }
+  for(let i=0; i<this.patLength; i++){
+    this.right[pat.charCodeAt(i)-97] = i;
+  }
 }
 
-var RK = function(){
+BM.prototype.search = function(s){
+  let skip = 0;
+  for(let i=0; i<=s.length-this.patLength; i+=skip){
+    skip = 0;
+    for(let j=this.patLength-1; j>=0; j--){
+      if(s.charCodeAt(i+j)!=this.pat.charCodeAt(j)){
+        skip = j-this.right[s.charCodeAt(i+j)-97];
+        skip = skip<1?1:skip;
+        break;
+      }
+    }
+    if(skip==0){
+      return i;
+    }
+  }
+  return s.length;
+}
 
+var Q = 997;
+var RM = 1;
+var RK = function(pat){
+  this.pat = pat;
+  this.patHashCode = 0;
+  for(let i=0; i<pat.length; i++){
+    this.patHashCode = ((this.patHashCode*R)%Q+pat.charCodeAt(i)-97)%Q;
+  }
+  for(let i=1; i<pat.length; i++){
+    RM = (RM*R)%Q;
+  }
+}
+
+RK.prototype.search = function(s){
+  let txtHash = 0;
+  for(let i=0; i< this.pat.length; i++){
+    txtHash = ((txtHash*R)%Q+s.charCodeAt(i)-97)%Q;
+  }
+  if(txtHash==this.patHashCode){
+    return 0;
+  }
+  for(let i=this.pat.length; i<s.length; i++){
+    console.log(txtHash, RM,i);
+    txtHash = (txtHash + Q - ((s.charCodeAt(i-this.pat.length)-97)*RM)%Q)%Q;
+    txtHash = (txtHash*R + s.charCodeAt(i)-97)%Q;
+    if(txtHash == this.patHashCode){
+      return i-this.pat.length+1;
+    }
+  }
+  return s.length
 }
 
 module.exports = {
--- Coding/Algorithm/Algorithm 2/week5-2 Substring/SubstringSpec.js
+++ Coding/Algorithm/Algorithm 2/week5-2 Substring/SubstringSpec.js
@@ -1,9 +1,15 @@
 var expect = require('chai').expect;
 var KMP = require('./Substring.js').KMP;
+var BM = require('./Substring.js').BM;
+var RK = require('./Substring.js').RK;
 
 describe('Substring',function(){
   it('Knuth-Morris-Pratt', function(){
     var kmp = new KMP('needle');
     console.log(kmp.search('inahaysdqwewqstacjneedleinajkqwe'));
+    var bm = new BM('needle');
+    console.log(bm.search('inahaysdqwewqstacjneedleinajkqwe'));
+    var rk = new RK('needle');
+    console.log(rk.search('inahaysdqwewqstacjneedleinajkqwe'));
   })
 })
